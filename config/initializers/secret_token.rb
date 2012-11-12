78873653f9303cc449b897e004f9bc68811d59ac
Dane Jorgensen <dane@bgbng.com>
2012-11-11 21:18:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yabe::Application.config.secret_token = 'aa9a4886a21d5106582c676b9bbd1f584021b4701395b51e22f9618d6f26fb925bb199952d35d749cf9404dac7d507e97491abe47760bbb5d2c1bfb1963e159b'
